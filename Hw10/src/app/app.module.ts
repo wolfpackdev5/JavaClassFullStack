d2ae10f6be2c70522cd8415b568ac78ba7194269
Devin Lewis <dglewis@umich.edu>
2022-06-09 09:44:53 -0500

    finished Hw 10

---
+++ Hw10/src/app/app.module.ts
@@ -0,0 +1,25 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { ChildComponent } from './app.child';
+import { HighlightDirective } from './app.highlight.directive';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ChildComponent,
+    HighlightDirective
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
